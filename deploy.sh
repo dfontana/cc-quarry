a8bed56f749a8ac305e7e690b3e90481a06fa7b5
Dylan Fontana <dyfontana@gmail.com>
2021-04-04 13:19:07 -0400

    Fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,17 @@ API_TOKEN=$(<.gh-secret)
 GH_GIST='d72ae5868a87adeb6345dbe6f041138d'
 
 script="
-import sys,json
-data = sys.stdin.read()
-print(json.dumps(data))
+import json, sys
+filename = sys.stdin.read().strip()
+file = open(filename, 'r').read()
+print(json.dumps(file))
 "
 
-CCQ=`echo $(<cc-quarry.lua) | python -c "$script"`
-CCQArg=`echo $(<api/cc-quarry-arguments.lua) | python -c "$script"`
-CCQPrim=`echo $(<api/cc-quarry-primitives.lua) | python -c "$script"`
-CCQUtil=`echo $(<api/cc-quarry-utils.lua) | python -c "$script"`
+CCQ=`echo cc-quarry.lua | python -c "$script"`
+CCQArg=`echo api/cc-quarry-arguments.lua | python -c "$script"`
+CCQPrim=`echo api/cc-quarry-primitives.lua | python -c "$script"`
+CCQUtil=`echo api/cc-quarry-utils.lua | python -c "$script"`
+
 
 curl \
   -X PATCH \
